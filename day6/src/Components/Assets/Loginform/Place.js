53ec0206be01668e4cd7671d2ef2a3d091d0dc18
inbasri123 <120014316+inbasri123@users.noreply.github.com>
2023-09-25 10:44:22 +0530

    Add files via upload

---
+++ day6/src/Components/Assets/Loginform/Place.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import"./Place.css";
+import Sidebar from '../Loginform/Sidebar';
+export default function Place() {
+  return (
+    <div>
+    <Sidebar/>
+    <div className='fun'>
+       <div className='sq'>
+      <div className='pine'></div>
+    </div>
+    <div className='fk'>
+    <p>Pine trees are a type of evergreen coniferous tree that belong to the genus Pinus, which is part of the Pinaceae family.These forests occur at elevations of 7,000-8,500 feet.
+    Pine forests usually occur on dry slopes so needles that fall under the trees decompose very slowly. This thick layer of pine needles is another factor that reduces the understory vegetation.
+    These trees are known for their distinctive needle-like leaves, which are usually bundled in clusters, and their woody cones that contain seeds.</p>
+    </div>
+
+    <div className='sq1'>
+    <div className='man'></div>
+    </div>
+    <div className='fk1'>
+    <p>Pichavaram is a unique and ecologically significant mangrove forest located in the southern Indian state of Tamil Nadu.
+    It is situated near the town of Chidambaram, in the Cuddalore district, and is known for its exceptional biodiversity and natural beauty.
+    Pichavaram is one of the largest mangrove forests in India and is renowned for its dense mangrove vegetation. </p>
+    </div>
+
+
+    <div className='sq2'>
+    <div className='gav'></div>
+    </div>
+    <div className='fk2'>
+    <p>Gavi is one of the most pristine eco-tourist spots that is well preserved and untainted by modern civilization 
+    and is among the most beautiful forest places to visit in Kerala. This natural beauty is located on the outskirts of Pathanamthitta.
+    You can spot some of the rare species of wildlife including Lion-tailed macaque, Nilgiri Tahr, elephants, and more than 260 species of exotic birds at the interiors of Gavi. It is a space owned and occupied by nature. </p>
+    </div>
+    
+     
+     <div className='sq3'>
+     <div className='mun'></div>
+     </div>
+     <div className='fk3'>
+     <p>Munnar forest ranges located amidst the hill folds of 
+     Western Ghats is one of the dense and largest forest places in Kerala. 
+     It is home to Eravikulam National Park which is Kerala’s first National Park. 
+     It was established to protect the rare species of Nilgiri Tahr and Neelakurinji. 
+     This forest range is rich with eco-diversity of evergreen and deciduous forests, grasslands, hills, medicinal and shrublands.  
+     This destination is a unique blend of picturesque beauty, a diverse eco-system, and rare species of flora and fauna. </p>
+     </div>
+    </div>
+    </div>
+
+  )
+}
